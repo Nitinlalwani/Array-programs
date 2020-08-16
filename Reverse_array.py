93d11ee893dda2c93f89805531defffe5ba3bc70
Nitin lalwani <nitindlalwani@gmail.com>
2020-08-16 15:53:26 +0530

    Create Reverse_array.py
    Multiple ways to reverse an array.

---
+++ Reverse_array.py
@@ -0,0 +1,28 @@
+#There are bunch of maethod we can use to reverse the array.
+
+#1. Reverse array using python reverse() method.
+
+arr = [10,20,30,40,50]
+
+#here I will be using reverse method
+
+arr.reverse()
+print(arr)
+
+#2. Reverse array using python slicing concept.
+
+arr = [60, 70, 80, 90, 100]
+
+arr = arr[::-1]
+print(arr)
+
+#3. Reverse array using python reversed() method.
+
+arr = [110,120,130,140,150]
+
+arr = list(reversed(arr))
+print(arr)
+
+
+
+
